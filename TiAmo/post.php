7f6dd5b12cee225c48099ddd2934796220235c28
那他 <1696674719@qq.com>
2017-07-04 13:53:35 +0800

    标签post-meta 重写,新增作者的小头像,和评论状况以及日期
    标签post-meta 重写,新增作者的小头像,和评论状况以及日期

--- TiAmo/post.php
+++ TiAmo/post.php
@@ -11,13 +11,24 @@
                     <article class="single-post-content row m0 post">
                         <header class="row post-header">
                             <h2 class="post-title"><?php $this->title(); ?></h2>
-                            <h5 class="post-meta">
-                                <a href="#" class="date"><?php $this->date('F j, Y');?></a>
-                                <span class="post-author"></span>
-                            </h5>
+                            <div class="post-meta">
+                                <div class="post-meta-left">
+                                    <div class="post-meta-left-head">
+                                        <img src="<?php echo 'https://secure.gravatar.com/avatar/'.md5($this->author->mail).'?s=100&r=G&d=mm'; ?>">
+                                    </div>
+                                    <div class="post-meta-left-er">
+                                        <span style="border-right:solid 1px #999; padding-right:5px;"><?php $this->date('F j, Y');?></span>
+                                        <span>GOD</span>
+                                    </div>
+                                </div>
+                                <div class="post-meta-right">
+                                    <span style="margin-left:5px;"><?php $this->commentsNum(_t('暂无评论'), _t('仅有 1 条评论'), _t('已有 %d 条评论')); ?></span>
+                                </div>
+                            </div>
                         </header>
                         <div id="post-id" class="post-content row">
                             <?php $this->content(); ?>
+                            <p><?php echo creativecommons(); ?></p>
                             <span class="tag-links">
                             <?php $this->tags(', ', true); ?>
                         </span>
